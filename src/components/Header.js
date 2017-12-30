92a08acc0bf3059e68d9495a17d8a36ec3b613dd
Shengjie Xu <shengjix12@gmail.com>
2017-12-30 15:26:36 -0800

    add style for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 
-const Header = ({ header='Cryptocurrency Tracker' }) => (
-  <View style={styles.container}>
-    <Text>{header}</Text>
+const Header = ({ headerContents='Cryptocurrency Tracker' }) => (
+  <View style={styles.headerContainer}>
+    <Text style={styles.header}>
+      {headerContents}
+    </Text>
   </View>
 );
 
 Header.propTypes = {
-  header: PropTypes.string
+  headerContents: PropTypes.string
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  headerContainer: {
+    display: 'flex',
+    marginTop: 55,
+    alignItems: 'center'
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 20
   }
 });
 
