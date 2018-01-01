e081dae983aba8e8fd6f946c6f603433bbedeec2
Shengjie Xu <shengjix12@gmail.com>
2017-12-31 17:13:32 -0800

    fix mutation bug

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
     isFetching: false,
-    data: null,
+    data: [],
     hasError: false,
     errorMessage: null
 }
@@ -14,7 +14,7 @@ import {
 export default function(state=initialState, action) {
     switch(action.type) {
         case FETCHING_COIN_DATA:
-            return Object.assign(state, {
+            return Object.assign({}, state, {
                 isFetching: true,
                 data: null,
                 hasError: false,
@@ -22,7 +22,7 @@ export default function(state=initialState, action) {
             });
         
         case FETCHING_COIN_DATA_SUCCESS:
-            return Object.assign(state, {
+            return Object.assign({}, state, {
                 isFetching: false,
                 data: action.payload,
                 hasError: false,
@@ -30,7 +30,7 @@ export default function(state=initialState, action) {
             });
         
         case FETCHING_COIN_DATA_FAIL:
-            return Object.assign(state, {
+            return Object.assign({}, state, {
                 isFetching: false,
                 data: null,
                 hasError: true,
